572d120c95631a1550cf1c24bf910d20e1ea1514
Sumit chouhan <sumitkhan.dhana@gmail.com>
2023-12-20 14:56:11 +0530

    add more cards

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 import { AboutCardItem } from "./common/Helper";
-// Define a type for the AboutCard item
+
 type AboutProps = {
   items: AboutCardItem[];
 };
 
 const About: React.FC<AboutProps> = ({ items }) => {
   return (
-    <div className="container xl:max-w-[1100px] 3xl:max-w-[1140px] mx-auto px-3 xl:px-0 mt-12 sm:mt-[80px]" id="about">
-      <div className="flex flex-col md:flex-row gap-8 md:gap-4 xl:gap-[80px] md:h-[603px] mb-12">
+    <div className="container xl:max-w-[1100px] 3xl:max-w-[1140px] mx-auto px-3 xl:px-0 mt-12 sm:mt-[40px]" id="about">
+      <div className="flex flex-wrap mb-12">
         {items.map((item, id) => (
-          <div className="md:w-6/12 w-full flex flex-col justify-between h-full" key={id}>
+          <div
+            className={`md:w-6/12 w-full flex flex-col justify-between h-full md:h-[580px] lg::h-[603px] mb-6 mt-5 sm:mt-8  md:mt-10 sm:mb-0 ${
+              id % 2 !== 0 ? 'lg:pl-10 md:pl-4' : 'lg:pr-10 md:lg:pr-4'
+            }`}
+            key={id}
+          >
             <div className="mb-4 md:mb-0">
-                <div className="overflow-hidden rounded">
-                  <img
-                    height={510}
-                    width={278}
-                    className="w-full hover:scale-110 transition-all duration-300"
-                    src={item.nature_img}
-                    alt="nature"
-                  />
-                </div>
-                <button className="ff_roboto text-[#DFE1E7] bg-[#283A61] font-medium text-sm py-[3px] px-4 rounded mt-5 mb-3 hover:bg-transparent border hover:border-[#283A61] hover:text-[#283A61] transition-all duration-300">
-                  {item.btn}
-                </button>
-                <h3 className="ff_Merriweather text-black font-bold leading-[140%] text-[22px] sm:text-2xl">
-                  {item.heading}
-                </h3>
-                <p className="ff_Merriweather text-[#515151] leading-[160%] pb-2 sm:pb-4">
-                  {item.timePara}
-                </p>
-                <p className="ff_roboto text-[#434343] text-base leading-[160%]">
-                  {item.mainPara}
-                </p>
+              <div className="overflow-hidden rounded">
+                <img
+                  height={510}
+                  width={278}
+                  className="w-full hover:scale-110 transition-all duration-300"
+                  src={item.nature_img}
+                  alt="nature"
+                />
+              </div>
+              <button className="ff_roboto text-[#DFE1E7] bg-[#283A61] font-medium text-sm py-[3px] px-4 rounded mt-5 mb-3 hover:bg-transparent border hover:border-[#283A61] hover:text-[#283A61] transition-all duration-300">
+                {item.btn}
+              </button>
+              <h3 className="ff_Merriweather text-black font-bold leading-[140%] text-[22px] sm:text-2xl">
+                {item.heading}
+              </h3>
+              <p className="ff_Merriweather text-[#515151] leading-[160%] pb-2 sm:pb-4">
+                {item.timePara}
+              </p>
+              <p className="ff_roboto text-[#434343] text-base leading-[160%]">
+                {item.mainPara}
+              </p>
             </div>
             <div className="flex items-center">
               <img height={42} width={42} src={item.bottomImg} alt="user" />
--- src/components/common/Helper.tsx
+++ src/components/common/Helper.tsx
@@ -35,6 +35,26 @@ export const items: AboutCardItem[] = [
     bottomImg: user_img,
     userName: "George Costanza",
   },
+  {
+    nature_img: food_img,
+    btn: "Food Theory",
+    heading: "Why You Should Never Order 12 Chicken Nuggets and Fries",
+    timePara: "Aug 1, 2021 • 7 min read",
+    mainPara:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam mollis lectus vitae nulla malesuada amet purus sed. A condimentum tempus a egestas sodales diam cras. Ligula a varius tempus ac amet, vel lectus sed. Urna sit Eget.",
+    bottomImg: user_img,
+    userName: "George Costanza",
+  },
+  {
+    nature_img: nature_card,
+    btn: "Travel",
+    heading: "What Traveling Greece For 2 Weeks Taught Me About Life",
+    timePara: "Jun 21, 2021 • 11 min read",
+    mainPara:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam mollis lectus vitae nulla malesuada amet purus sed. A condimentum tempus a egestas sodales diam cras.",
+    bottomImg: user_img,
+    userName: "George Costanza",
+  },
 ];
 
 // food theory data
